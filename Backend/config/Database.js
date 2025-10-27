069bfff2222e72113b3ce02466be43e61a1f60cf
Randa alfizah <160241075+RndaAfzh15@users.noreply.github.com>
2025-10-27 15:12:40 +0700

    Update Database.js

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,10 +1,14 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 
-const db = new Sequelize('crud_db', 'root','', {
-    host:'localhost',
-    port:'3306',
-    dialect: 'mysql'
-});
-
+const db = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql",
+  }
+);
 
 export default db;
