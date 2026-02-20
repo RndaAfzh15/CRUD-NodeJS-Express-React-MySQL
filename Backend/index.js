faa4bf056341f38db0b105c7cae273714545be9e
alpin.dev <160241075+RndaAfzh15@users.noreply.github.com>
2026-02-20 15:05:33 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,37 +8,27 @@ dotenv.config();
 
 const app = express();
 
-/*
-  ===== CORS (SIMPLE MODE DULU) =====
-*/
-app.use(cors()); // sementara buka semua biar pastiin bukan CORS config yg salah
+
+app.use(cors()); 
 
 app.use(express.json());
 
-/*
-  ===== TEST ROOT =====
-*/
+
 app.get("/", (req, res) => {
   res.send("API RUNNING 🚀");
 });
 
-/*
-  ===== ROUTES =====
-*/
+
 app.use("/users", UserRoute);
 
-/*
-  ===== START SERVER DULU =====
-*/
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("SERVER STARTED ON", PORT);
 });
 
-/*
-  ===== CONNECT DATABASE (BELAKANGAN) =====
-*/
+
 (async () => {
   try {
     await db.authenticate();
@@ -47,3 +37,4 @@ app.listen(PORT, "0.0.0.0", () => {
     console.error("DB connection failed:", error);
   }
 })();
+
