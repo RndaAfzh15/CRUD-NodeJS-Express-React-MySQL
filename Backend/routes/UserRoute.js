988beefb3df3ae6b6e5ad5155840c534499c3566
alpin.dev <160241075+RndaAfzh15@users.noreply.github.com>
2026-02-20 15:05:01 +0700

    Update UserRoute.js

--- Backend/routes/UserRoute.js
+++ Backend/routes/UserRoute.js
@@ -9,19 +9,16 @@ import {
 
 const router = express.Router();
 
-// GET all users
 router.get("/", getUsers);
 
-// GET user by id
+
 router.get("/:id", getUsersById);
 
-// CREATE user
 router.post("/", createUser);
 
-// UPDATE user
 router.patch("/:id", updateUser);
 
-// DELETE user
 router.delete("/:id", deleteUser);
 
 export default router;
+
